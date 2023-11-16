a419c126ea9534a0eb9fd9fff047634cef8ef017
강준규 <wnsrb933@naver.com>
2023-11-16 14:22:32 +0900

    ver 1.2

--- Backend/BigStep/src/main/java/com/ssafy/bigstep/controller/LikeController.java
+++ Backend/BigStep/src/main/java/com/ssafy/bigstep/controller/LikeController.java
@@ -1,5 +1,12 @@
 package com.ssafy.bigstep.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/like")
+@CrossOrigin("*")
 public class LikeController {
 
 }
--- Backend/BigStep/src/main/java/com/ssafy/bigstep/model/dao/UserDao.java
+++ Backend/BigStep/src/main/java/com/ssafy/bigstep/model/dao/UserDao.java
@@ -1,17 +1,25 @@
 package com.ssafy.bigstep.model.dao;
 
+import java.util.List;
+
+import com.ssafy.bigstep.model.dto.User;
+
 public interface UserDao {
-	
-	
-	//insert
-	
-	//delete(회원탈퇴)
-	
-	//update
-	
-	//SelectAll
-	
-	//SelectOne
-	
-	
+
+	// insert
+	public int insertUser(User user);
+
+	// delete(회원탈퇴)
+
+	public int deleteUser(String id);
+
+	// update
+	public int updateUser(String id);
+
+	// SelectAll
+	public List<User> selectUsers();
+
+	// SelectOne
+	public User selectUser(String id);
+
 }
