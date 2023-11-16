752cb147f0ff2835701af455aa8f297ec50bbad8
고은석 <ko2sist@gmail.com>
2023-11-16 17:41:10 +0900

    ver 1.41

--- Backend/BigStep/src/main/java/com/ssafy/bigstep/controller/CommentController.java
+++ Backend/BigStep/src/main/java/com/ssafy/bigstep/controller/CommentController.java
@@ -1,17 +1,94 @@
 package com.ssafy.bigstep.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.bigstep.model.dto.Comment;
 import com.ssafy.bigstep.model.service.CommentService;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
-@RequestMapping("/comment")
+@RequestMapping("/comment-api")
 @CrossOrigin("*")
 public class CommentController {
 	
 	@Autowired
 	CommentService cService;
+	
+	public ResponseEntity<?> exceptionHandling(Exception e) {
+		return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+
+	@GetMapping("/comments/{diaryId}")
+	@ApiOperation(value="댓글 목록 조회", notes="{diaryId}에 해당하는 diary에 대한 댓글 목록을 조회한다.")
+	public ResponseEntity<?> getList(@PathVariable int diaryId) {
+		try {
+			List<Comment> list = cService.selectComments(diaryId);
+			if(list == null) {
+				return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+			}else {
+				return new ResponseEntity<List<Comment>>(list, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+	}
+	
+	@GetMapping("/comment/{commentId}")
+	@ApiOperation(value="댓글 상세 조회", notes="{commentId}에 해당하는 댓글의 상세 정보를 조회한다.")
+	public ResponseEntity<?> getDetail(@PathVariable int commentId){
+		try {
+			Comment comment = cService.selectComment(commentId);
+			if(comment == null) {
+				return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+			}else {
+				return new ResponseEntity<Comment>(comment, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+	}
+	
+	@PostMapping("/comment")
+	@ApiOperation(value="댓글 등록", notes="JSON 형태로 입력받은 댓글을 등록한다.")
+	public ResponseEntity<?> insertComment(@RequestBody Comment comment){
+		try {
+			int result = cService.insertComment(comment);
+			if(result == 0) {
+				return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+			}else {
+				return new ResponseEntity<Comment>(comment, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+	}
+	
+	@PutMapping("/comment")
+	@ApiOperation(value="댓글 수정", notes="JSON 형태로 입력받은 댓글 정보를 바탕으로 댓글을 수정한다.")
+	public ResponseEntity<?> updateComment(@RequestBody Comment comment){
+		try {
+			int result = cService.updateComment(comment);
+			if(result == 0) {
+				return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+			}else {
+				return new ResponseEntity<Comment>(comment, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return exceptionHandling(e);
+		}
+	}
 }
--- Backend/BigStep/src/main/java/com/ssafy/bigstep/controller/DiaryController.java
+++ Backend/BigStep/src/main/java/com/ssafy/bigstep/controller/DiaryController.java
@@ -1,6 +1,8 @@
 package com.ssafy.bigstep.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +17,8 @@ public class DiaryController {
 	@Autowired
 	DiaryService dService;
 	
+	public ResponseEntity<?> exceptionHandling(Exception e) {
+		return new ResponseEntity<Void>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 	
 }
