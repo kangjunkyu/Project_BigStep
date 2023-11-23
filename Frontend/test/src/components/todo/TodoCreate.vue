0c1806967f91f5c3daeafa719befbb427d667d03
강준규 <wnsrb933@naver.com>
2023-11-23 16:52:13 +0900

    test_1652

--- Frontend/test/src/components/todo/TodoCreate.vue
+++ Frontend/test/src/components/todo/TodoCreate.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="w-380">
     <div class="flex flex-row justify-between">
-      <p class="text-xl font-bold">Update Todo</p>
+      <p class="text-xl font-bold">Create Todo</p>
 
       <button
         @click="createTodo"
         class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
       >
-        Modify
+        Create
       </button>
     </div>
     <div class="pt-5">
